e63c9a4abc7c83692e9f38a0b5c4b353899360a9
Valeriia Tulchinskaia <72628164+valaary@users.noreply.github.com>
2025-06-18 12:34:12 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -5,3 +5,11 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active');  // анимация иконки
   navbar.classList.toggle('active');  // показать/скрыть меню
 });
+
+// Для доступности — открытие меню по клавише Enter или пробел
+burger.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' || e.key === ' ') {
+    e.preventDefault();
+    burger.click();
+  }
+});
